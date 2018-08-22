5a366ab84665c04207f84d5a5547f958856cf4de
Lis Don <s.lis.don@gmail.com>
2018-08-22 21:54:12 +0600

    Тестовый QT5-проект для rpi0

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->btnExit, SIGNAL(clicked(bool)),
+            this, SLOT(close())
+            );
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
