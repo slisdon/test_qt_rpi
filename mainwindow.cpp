cc749967c52adb9863f4b8868d01b8d3c6e520db
Lis Don <s.lis.don@gmail.com>
2018-09-08 11:32:53 +0600

    Добавлена функция тестового проигрывания IPTV-потока через omxplayer при нажатии на кнопку

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,12 +7,35 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    /// \brief Настройки кнопок
+    ui->btnExit->setStyleSheet(styleButtonActive);
+    ui->btn1->setStyleSheet(styleButtonActive);
+
+    ui->btnExit->setEnabled(true);
+    ui->btn1->setEnabled(true);
+
+    ui->btnExit->setText("ВЫХОД");
+    ui->btnExit->setFixedWidth(200);
+    ui->btnExit->setFixedHeight(50);
+
+    ui->btn1->setText("Кинохит");
+    ui->btn1->setFixedWidth(200);
+    ui->btn1->setFixedHeight(50);
+
     connect(ui->btnExit, SIGNAL(clicked(bool)),
             this, SLOT(close())
+            );
+
+    connect(ui->btn1, SIGNAL(clicked(bool)),
+            this, SLOT(playTV())
             );
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::playTV() {
+    system("~/iptv.sh omxplayer --aspect-mode fill -r -d -y -w -o hdmi http://50.7.136.155:8081/kinohit/index.m3u8");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,31 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QString styleButtonActive = "QPushButton {"
+                        "border-style: outset;"
+                        "border-width: 4px;"
+                        "border-radius: 10px;"
+                        "border-color: yellow;"
+                        "background-color: blue;"
+                        "font: bold 20px;"
+                        "padding: 6px;"
+                        "selection-background-color: yellow;"
+                        "}";
+    QString styleButtonInactive = "QPushButton {"
+                        "border-style: outset;"
+                        "border-width: 4px;"
+                        "border-radius: 10px;"
+                        "border-color: yellow;"
+                        "background-color: white;"
+                        "font: bold 20px;"
+                        "padding: 6px;"
+                        "selection-color: yellow;"
+                        "selection-background-color: yellow;"
+                        "}";
+
+public Q_SLOTS:
+    void playTV();
 };
 
 #endif // MAINWINDOW_H
